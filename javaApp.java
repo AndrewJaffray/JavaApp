a607ea153d528e742345e97d5552cf7b08757b0e
AndrewJaffray <130483685+AndrewJaffray@users.noreply.github.com>
2023-10-04 12:18:30 +0100

    Update javaApp.java

--- javaApp.java
+++ javaApp.java
@@ -1,15 +1,36 @@
 package JavaScanner;
 
-import java.util.scanner;
+import java.util.Scanner;
+
+
 
 public class javaApp {
-public static void main (String [] args) {
 
+public static void main(String [] args) {
 	
+int firstNumber;
+int secondNumber;
+
+
+Scanner sc = new Scanner(System.in);	
+System.out.print("Would you like to add or multiply?: ");	
+String operation = sc.next();
+
+if operation = "add" 
+	(firstNumber + secondNumber);
+
+
+
+if String operation = "multiply"
+	(firstNumber * secondNumber);
 	
+System.out.print("Enter the first number: ");	
+ firstNumber = sc.nextInt();
+System.out.print("Enter the second number: ");
+ secondNumber = sc.nextInt();
 	
-	
-	
+int result = performOperation( firstNumber,  secondNumber, operation);
+	System.out.println("The result is: " + result);	
 	
 	
 	
