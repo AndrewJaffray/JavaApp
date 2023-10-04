f35d466677efa2a48268802827eecea91a247fd1
AndrewJaffray <130483685+AndrewJaffray@users.noreply.github.com>
2023-10-04 12:24:08 +0100

    Update javaApp.java
    fixed and done

--- javaApp.java
+++ javaApp.java
@@ -15,21 +15,26 @@ int secondNumber;
 Scanner sc = new Scanner(System.in);	
 System.out.print("Would you like to add or multiply?: ");	
 String operation = sc.next();
-
-if operation = "add" 
-	(firstNumber + secondNumber);
-
-
-
-if String operation = "multiply"
-	(firstNumber * secondNumber);
 	
+
 System.out.print("Enter the first number: ");	
  firstNumber = sc.nextInt();
 System.out.print("Enter the second number: ");
  secondNumber = sc.nextInt();
 	
-int result = performOperation( firstNumber,  secondNumber, operation);
+ int result = 0;
+ 
+ 
+if (operation.equals("add")) { 
+	result = firstNumber + secondNumber;
+}else if (operation.equals("multiply")) {
+	result = firstNumber * secondNumber;
+}else if (operation.equals("exit"))	{
+	System.exit(1);
+} else {
+	System.out.println("Invalid operation.");
+	System.exit(1);
+}
 	System.out.println("The result is: " + result);	
 	
 	
